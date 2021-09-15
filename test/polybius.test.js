8382516b41b4ca55577b785830e78d3b70c3b9cb
Devante Trotman <devantetrotman@gmail.com>
2021-09-14 22:48:53 -0500

    completed polybius.test.js

--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,1 +1,22 @@
-// Write your tests here!
+const { expect } = require("chai");
+const {polybius} = require("../src/polybius");
+
+describe("polybius", () => {
+  it("should encode an input string into a string of numbers", () => {
+    let input = "Hello world";
+    
+    const actual = polybius(input);
+    const expected = '3251131343 2543241341';
+    //console.log(actual);
+    //console.log(expected);
+    expect(actual).to.eql(expected);
+  });
+  
+  it("should decode an input of a string of numbers into an alphabetic string.", () => {
+    const input = '3251131343 2543241341';
+    
+    const actual = polybius(input, false);
+    const expected = "hello world";
+    expect(actual).to.eql(expected);
+     });
+});
