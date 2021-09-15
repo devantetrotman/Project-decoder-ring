2fb3dbef090057d1b46e536db8828622cbb0fa49
Devante Trotman <devantetrotman@gmail.com>
2021-09-14 22:50:12 -0500

    completed substitution.test.js

--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,24 @@
-// Write your tests here!
+const { expect } = require("chai");
+const {substitution} = require("../src/substitution");
+
+describe("substitution", () => {
+  it("should use the appropriate alphabet to encode input", () => {
+    let input = "thinkful";
+    const alphabet = "xoyqmcgrukswaflnthdjpzibev";
+    
+    const actual = substitution(input, alphabet);
+    const expected = 'jrufscpw';
+    //console.log(actual);
+    //console.log(expected);
+    expect(actual).to.eql(expected);
+  });
+  
+  it("should use the appropriate alphabet to decode input", () => {
+    const input = "jrufscpw";
+    const alphabet = "xoyqmcgrukswaflnthdjpzibev";
+    
+    const actual = substitution(input, alphabet, false);
+    const expected = "thinkful";
+    expect(actual).to.eql(expected);
+     });
+});
