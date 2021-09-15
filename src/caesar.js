305c1a3d0f08f5287edb3c8ee6a252712185a405
Devante Trotman <devantetrotman@gmail.com>
2021-09-14 22:41:56 -0500

    completed caesar.js

--- src/caesar.js
+++ src/caesar.js
@@ -9,20 +9,69 @@ const caesarModule = (function () {
   function caesar(input, shift, encode = true) {
     let encoded = [];
     let message;
+    if (encode == false){
+      if (shift == 0 || shift < -25 || shift > 25){
+      return false
+    }
+    else{
+      for (let i = 0; i < input.length; i++){
+        let ascii = input[i].charCodeAt();
+        if (ascii <= 64 || (ascii > 90 && ascii < 97) || ascii > 122){
+          let codeSymbol = String.fromCharCode(ascii);
+          console.log(codeSymbol);
+          encoded.push(codeSymbol);
+        } else {
+          if(ascii >= 65 && ascii <= 90){
+            ascii += 32; 
+          }
+          let shiftedValue = ascii - shift;
+          if (shiftedValue > 122) {
+            shiftedValue = shiftedValue-122+96;
+          }
+          if (shiftedValue<97) {
+            shiftedValue = shiftedValue+122-96;
+          }
+          let codeLetter = String.fromCharCode(shiftedValue);
+          //console.log(codeLetter);
+          encoded.push(codeLetter);
+        }
+      }
+    }
+    message = encoded.join("");
+   // console.log(encoded);
+    console.log(message);
+    return message
+    }
+    
     if (shift == 0 || shift < -25 || shift > 25){
       return false
     }
     else{
       for (let i = 0; i < input.length; i++){
         let ascii = input[i].charCodeAt();
-        if (ascii == 32){break;}
-        let shiftedValue = ascii + shift;
-        let codeLetter = String.fromCharCode(shiftedValue);
-        encoded.push(codeLetter);
+        if (ascii <= 64 || (ascii > 90 && ascii < 97) || ascii > 122){
+          let codeSymbol = String.fromCharCode(ascii);
+          console.log(codeSymbol);
+          encoded.push(codeSymbol);
+        } else {
+          if(ascii >= 65 && ascii <= 90){
+            ascii += 32; 
+          }
+          let shiftedValue = ascii + shift;
+          if (shiftedValue > 122) {
+            shiftedValue = shiftedValue-122+96;
+          }
+          if (shiftedValue<97) {
+            shiftedValue = shiftedValue+122-96;
+          }
+          let codeLetter = String.fromCharCode(shiftedValue);
+          //console.log(codeLetter);
+          encoded.push(codeLetter);
+        }
       }
     }
     message = encoded.join("");
-    console.log(encoded);
+   // console.log(encoded);
     console.log(message);
     return message
   }
